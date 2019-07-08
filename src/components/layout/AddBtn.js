ac4d84b26c79c5a2d9351b7cd98678e08b0699fc
angel <angelsol299@hotmail.com>
2019-07-08 21:58:53 +0200

    modals

--- src/components/layout/AddBtn.js
+++ src/components/layout/AddBtn.js
@@ -7,8 +7,23 @@ const AddBtn = () => {
         href="#add-log-modal"
         className="btn-floating btn-large blue darken-2 modal-trigger"
       >
-        <i className="large material-icons">Add</i>
+        <i className="large material-icons">add</i>
       </a>
+      <ul>
+        <li>
+          <a
+            href="#tech-list-modal"
+            className="btn-floating green modal-trigger"
+          >
+            <i className="material-icons">person</i>
+          </a>
+        </li>
+        <li>
+          <a href="#tech-modal" className="btn-floating red modal-trigger">
+            <i className="material-icons">person_add</i>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
