5762eb3ca3e76a27d91589d17bcbc05ce67a78f7
angel <angelsol299@hotmail.com>
2019-07-09 18:55:38 +0200

    delete logs

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Moment from 'react-moment';
 const LogItem = ({ log }) => {
   return (
